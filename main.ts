a905d32dc93d6571189c943c3acbcd72bc3e8239
dragonbiteSniper <167490855+dragonbiteSniper@users.noreply.github.com>
2024-04-18 21:45:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinPressed(TouchPin.P0, function() {
+ music.ringTone(Note.C)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.ringTone(Note.D)
+})
+
+input.onPinPressed(TouchPin.P2, function () {
+    music.ringTone(Note.E)
+})
+input.onPinPressed(TouchPin.P3, function () {
+    music.ringTone(Note.F)
+})
